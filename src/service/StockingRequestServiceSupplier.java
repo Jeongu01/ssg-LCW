0b1e1fb2efb94b4a613d28dc501a75c25ca12026
hyo9634 <dlgytmd5468@naver.com>
2024-08-14 10:51:34 +0900

    review accepted

--- src/service/StockingRequestServiceSupplier.java
+++ src/service/StockingRequestServiceSupplier.java
@@ -6,7 +6,7 @@ import vo.StockingRequestVO;
 public interface StockingRequestServiceSupplier {
 
   //입고 요청
-  public void RequestWarehousing(StockingRequestVO data);
+  public void requestWarehousing(StockingRequestVO data);
 
   //내 입고 요청서 조회 -> userid를 필요로 함
   public ArrayList<StockingRequestVO> inquiryWarehouseRequests();
