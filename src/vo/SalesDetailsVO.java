77c8d90a481062457a83dcd499f5710ae344c98f
wanna175 <jh5045@naver.com>
2024-08-14 15:04:46 +0900

    ✨ feat  : 재무관리 기능구현중

---
+++ src/vo/SalesDetailsVO.java
@@ -0,0 +1,16 @@
+package vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+public class SalesDetailsVO {
+    private int salesDetailsId;
+    private Date salesDetailsDate;
+    private int cost;
+    private int categoryId;
+    private int warehouseId;
+}
