4b7edc1a11dc658745f945811af5fc86624dc7d7
ㅅㅎㅇ <ise0305@naver.com>
2024-08-14 17:49:26 +0900

    ✨ feat  : 재고 기능 중

---
+++ src/service/StocktakingImpl.java
@@ -0,0 +1,87 @@
+package service;
+
+import java.util.List;
+import vo.StockCountVO;
+import vo.StockVO;
+
+public class StocktakingImpl implements StocktakingInterface{
+
+  private StocktakingDAO dao = null;
+
+  public StocktakingImpl(){
+    dao = new StocktakingDAO();
+  }
+
+  /*관리자급 재고 출력*/
+  //전체 재고 목록 출력
+  //카테고리별 재고 목록 출력
+  //검색어로 재고 목록 출력
+  //회원별 재고 목록 출력
+
+
+  @Override
+  public List<StockVO> wholeStockList() {
+    return List.of();
+  }
+
+  @Override
+  public List<StockVO> majorStockList(int categoryId) {
+    return List.of();
+  }
+
+  @Override
+  public List<StockVO> middleStockList(int categoryId) {
+    return List.of();
+  }
+
+  @Override
+  public List<StockVO> smallStockList(int categoryId) {
+    return List.of();
+  }
+
+  @Override
+  public List<StockVO> keywordStockList(String keyword) {
+    return List.of();
+  }
+
+  @Override
+  public List<StockVO> userStockList(int userId) {
+    return List.of();
+  }
+
+  @Override
+  public StockCountVO editStockCount(int productId) {
+    return null;
+  }
+
+  @Override
+  public void deleteStockCount(int productId) {
+
+  }
+
+  @Override
+  public void uploadStockCount(int productId, String productName, int quantity) {
+
+  }
+
+  @Override
+  public void minusStock(boolean result) {
+
+  }
+
+  @Override
+  public void plusStock(boolean result) {
+
+  }
+
+  @Override
+  public List<StockVO> warehouseStatus() {
+    return List.of();
+  }
+
+  @Override
+  public void createStockLog() {
+
+
+  }
+}
