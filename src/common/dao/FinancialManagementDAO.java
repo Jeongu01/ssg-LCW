6811bec44af5d4fe93b02c586bae215170550e04
wanna175 <jh5045@naver.com>
2024-08-14 16:53:09 +0900

    ✨ feat  : 구조짜는중4

--- src/common/dao/FinancialManagementDAO.java
+++ src/common/dao/FinancialManagementDAO.java
@@ -1,4 +1,50 @@
 package common.dao;
 
+import lib.ConnectionPool;
+import vo.ContractVO;
+import vo.ExpenditureDetailsVO;
+import vo.SalesDetailsVO;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
 public class FinancialManagementDAO {
+    private ConnectionPool conncp = null;
+    private Connection connection = null;
+
+    public FinancialManagementDAO(){
+        init();
+    }
+
+    private void init(){
+        conncp = ConnectionPool.getInstance();
+    }
+
+    public ArrayList<ContractVO> selectContracts(String where){
+        return null;
+    }
+    public void insertContract(ContractVO data){
+
+    }
+    public void updateContract(ContractVO data){
+
+    }
+    public void deleteContract(ContractVO data){
+
+    }
+    public ArrayList<ExpenditureDetailsVO> selectAllExpDet(){
+        return null;
+    }
+    public ArrayList<SalesDetailsVO> selectAllSalDet(){
+        return null;
+    }
+    public ArrayList<ExpenditureDetailsVO> selectExpDet(String where){
+        return null;
+    }
+    public ArrayList<SalesDetailsVO> selectSalDet(String where){
+        return null;
+    }
+    public void insertExpDet(ExpenditureDetailsVO data){}
+    public void updateExpDet(ExpenditureDetailsVO data){}
+    public void deleteExpDet(ExpenditureDetailsVO data){}
 }
--- src/service/FMserviceGM.java
+++ src/service/FMserviceGM.java
@@ -1,4 +1,15 @@
 package service;
 
-public interface FMserviceGM {
+import vo.ExpenditureDetailsVO;
+import vo.RevenueVO;
+import vo.SalesDetailsVO;
+
+import java.util.ArrayList;
+import java.sql.Date;
+
+public interface FMserviceGM extends FMserviceWM{
+    public ArrayList<ExpenditureDetailsVO> inquiryAllExpDet();
+    public ArrayList<SalesDetailsVO> inquiryAllSalDet();
+    public RevenueVO inquiryTotalRevenue(Date date);
+
 }
